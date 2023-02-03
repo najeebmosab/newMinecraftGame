e062c547c70cd7bae91828c5dd8d2e0c806408e9
najeebmosab <73877639+najeebmosab@users.noreply.github.com>
2023-02-03 22:10:26 +0200

    new minecraft game

--- index.js
+++ index.js
@@ -15,53 +15,63 @@ console.log(yllowBlock);
 for (let row = 0; row < 15; row++) {
     for (let col = 0; col < 15; col++) {
         let img = document.createElement("img");
-       
+        let baicsDiv = document.createElement("div");
+
         if (col == wood && row >= 9 + numOfLevel && row < 12) {
             img.src = woodImg;
-            img.style.gridColumn = col + 1;
-            img.style.gridRow = row + 1;
-            gameScreen.appendChild(img);
+            //    img.style.gridColumn = col + 1;
+            //    img.style.gridRow = row + 1;
+            baicsDiv.appendChild(img);
+            baicsDiv.style.gridColumn = col + 1;
+            baicsDiv.style.gridRow = row + 1;
+            gameScreen.appendChild(baicsDiv);
         }
-        else if(col <= yllowBlock-2 && row <=yllowBlock-2){
+        else if (col <= yllowBlock - 2 && row <= yllowBlock - 2) {
             let div = document.createElement("div");
-            div.style.backgroundColor="yellow";
-            div.style.gridColumn = col + 1;
-            div.style.gridRow = row + 1;
-            gameScreen.appendChild(div);
+            div.style.backgroundColor = "yellow";
+            baicsDiv.appendChild(div);
+            baicsDiv.style.gridColumn = col + 1;
+            baicsDiv.style.gridRow = row + 1;
+            gameScreen.appendChild(baicsDiv);
         }
         else if ((col == wood - 1 && row >= 9 - 2 && row < 12 + numOfLevel - 3) || (col == wood && row >= 9 - 2 && row < 12 + numOfLevel - 3) || (col == wood + 1 && row >= 9 - 2 && row < 12 + numOfLevel - 3)) {
             img.src = Convite;
-            img.style.gridColumn = col + 1;
-            img.style.gridRow = row + 1;
-            gameScreen.appendChild(img);
+            baicsDiv.appendChild(img);
+            baicsDiv.style.gridColumn = col + 1;
+            baicsDiv.style.gridRow = row + 1;
+            gameScreen.appendChild(baicsDiv);
         }
         else if (col == blockFlot && row >= 9 + numOfLevel && row < 12) {
 
             img.src = Spoonflower;
-            img.style.gridColumn = col + 1;
-            img.style.gridRow = row + 1;
-            gameScreen.appendChild(img);
+            baicsDiv.appendChild(img);
+            baicsDiv.style.gridColumn = col + 1;
+            baicsDiv.style.gridRow = row + 1;
+            gameScreen.appendChild(baicsDiv);
 
         }
 
         else if (row < 12) {
             let div = document.createElement("div");
             div.style.backgroundColor = "#96d6f2";
-            gameScreen.appendChild(div);
-            div.style.gridColumn = col + 1;
-            div.style.gridRow = row + 1;
+            baicsDiv.appendChild(div);
+            baicsDiv.style.gridColumn = col + 1;
+            baicsDiv.style.gridRow = row + 1;
+            gameScreen.appendChild(baicsDiv);
         }
         else if (row == 12) {
             img.src = gras;
-            img.style.gridColumn = col + 1;
-            img.style.gridRow = row + 1;
-            gameScreen.appendChild(img);
+            baicsDiv.appendChild(img);
+            baicsDiv.style.gridColumn = col + 1;
+            baicsDiv.style.gridRow = row + 1;
+            gameScreen.appendChild(baicsDiv);
         }
         else if (row > 12) {
             img.src = flot;
-            img.style.gridColumn = col + 1;
-            img.style.gridRow = row + 1;
-            gameScreen.appendChild(img);
+            baicsDiv.appendChild(img);
+            baicsDiv.style.gridColumn = col + 1;
+            baicsDiv.style.gridRow = row + 1;
+            gameScreen.appendChild(baicsDiv);
         }
 
 
